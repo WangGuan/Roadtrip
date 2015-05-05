23cf1727fffb1a4378060f30f9623a71145aec9e
Rhino <793797714@qq.com>
2015-05-05 11:35:19 +0800

    初始化项目

---
+++ src/com/wy/roadtrip/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.wy.roadtrip.activity;
+
+import com.froyo.commonjar.activity.BaseActivity;
+import com.wy.roadtrip.R;
+/**
+ * 
+ * @Des: 首页
+ * @author Rhino 
+ * @version V1.0 
+ * @created  2015年5月5日 上午10:19:16
+ */
+public class MainActivity extends BaseActivity {
+
+	@Override
+	protected int setLayoutResID() {
+		return R.layout.activity_main;
+	}
+}
