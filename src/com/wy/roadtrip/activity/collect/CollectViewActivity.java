5d62816ac5d2c833c719e4ccb5311bad16c11185
Rhino <793797714@qq.com>
2015-05-13 14:11:43 +0800

    讲线路看点修改为ExpandListView

--- src/com/wy/roadtrip/activity/collect/CollectViewActivity.java
+++ src/com/wy/roadtrip/activity/collect/CollectViewActivity.java
@@ -1,8 +1,23 @@
 package com.wy.roadtrip.activity.collect;
 
-import com.froyo.commonjar.activity.BaseActivity;
-import com.wy.roadtrip.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.ExpandableListView.OnChildClickListener;
+import android.widget.ExpandableListView.OnGroupClickListener;
+import android.widget.ExpandableListView.OnGroupExpandListener;
+
+import com.froyo.commonjar.activity.BaseActivity;
+import com.froyo.commonjar.utils.Utils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+import com.wy.roadtrip.R;
+import com.wy.roadtrip.adapter.CollectViewAdapter;
+import com.wy.roadtrip.vo.CollectViewChildVo;
+import com.wy.roadtrip.vo.CollectViewPvo;
+import com.froyo.commonjar.view.CustomExpandableListView;
 /**
  * 收藏---看点---详情
  * @author wangyi
@@ -10,6 +25,85 @@ import com.wy.roadtrip.R;
  */
 public class CollectViewActivity extends BaseActivity {
 
+	@ViewInject(R.id.ex_list)
+	private CustomExpandableListView ex_list;
+	
+	private  CollectViewAdapter adapter;
+	
+	/** 控制列表的展开，用于每次都定位于展开的项 */
+	private int sign = -1;
+	
+	@Override
+	public void doBusiness() {
+		adapter=new CollectViewAdapter(activity, new ArrayList<CollectViewPvo>(), R.layout.expand_list_item, R.layout.expand_list_child_item);
+		ex_list.setAdapter(adapter);
+		for (int i = 0; i < 10; i++) {
+			CollectViewPvo vo=new CollectViewPvo();
+			vo.setTitle("看点"+i);
+			List<CollectViewChildVo> childs=new ArrayList<CollectViewChildVo>();
+			for (int j = 0; j < 5; j++) {
+				CollectViewChildVo temp=new CollectViewChildVo();
+				temp.setContent("小看点"+i+" "+j);
+				childs.add(temp);
+			}
+			vo.setChild(childs);
+			adapter.addParent(vo);
+		}
+		ex_list.setOnChildClickListener(new OnChildClickListener() {
+
+			@Override
+			public boolean onChildClick(ExpandableListView parent, View v,
+					int groupPosition, int childPosition, long id) {
+				CollectViewChildVo item = (CollectViewChildVo) adapter.getChild(groupPosition,
+						childPosition);
+				toast(item.getContent());
+				return true;
+			}
+		});
+		// 控制list只展开一项
+		ex_list.setOnGroupExpandListener(new OnGroupExpandListener() {
+
+			@Override
+			public void onGroupExpand(int groupPosition) {
+				for (int i = 0; i < adapter.getGroupCount(); i++) {
+					if (groupPosition != i) {
+						ex_list.collapseGroup(i);
+					}else{
+						
+					}
+				}
+			}
+		});
+		// 控制list展开的一项为最前面
+		ex_list.expandGroup(0);
+		ex_list.setOnGroupClickListener(new OnGroupClickListener() {
+			@Override
+			public boolean onGroupClick(ExpandableListView parent, View v,
+					int groupPosition, long id) {
+				if (adapter.hasChild(groupPosition)) {
+					if (sign == -1) {
+						ex_list.expandGroup(groupPosition);
+						ex_list.setSelectedGroup(groupPosition);
+						sign = groupPosition;
+					} else if (sign == groupPosition) {
+						ex_list.collapseGroup(groupPosition);
+						sign = -1;
+					} else {
+						ex_list.collapseGroup(sign);
+						ex_list.expandGroup(groupPosition);
+						ex_list.setSelectedGroup(groupPosition);
+						sign = groupPosition;
+					}
+					return true;
+				} else {
+					return false;
+				}
+			}
+		});
+		
+	}
+	
+	
 	@Override
 	protected int setLayoutResID() {
 		return R.layout.activity_collect_view;
---
+++ src/com/wy/roadtrip/vo/CollectViewChildVo.java
@@ -0,0 +1,15 @@
+package com.wy.roadtrip.vo;
+
+public class CollectViewChildVo {
+	
+	private String content;
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
